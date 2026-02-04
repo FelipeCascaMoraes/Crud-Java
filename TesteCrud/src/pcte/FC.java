b54ed2f5bdb467e45b546f65841c20b7b4162ad4
Felipe Cascaioli de Moraes <felipe.casca.moraes@gmail.com>
2026-02-04 20:55:54 -0300

    Primeiro commit - CRUD Java Swing

---
+++ TesteCrud/src/pcte/FC.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pcte;
+
+import java.io.File;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+/**
+ *
+ * @author Usuario
+ */
+class FC {
+
+    static void setAcceptAllFileFilterUsed(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setFileFilter(FileNameExtensionFilter fileNameExtensionFilter) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static int showOpenDialog(Object object) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static File getSelectedFile() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
